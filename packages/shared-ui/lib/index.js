f40d04e954fbe0cc36295b213331dc3981f78c4d
alberto <alberto.soto@gmail.com>
2021-05-03 08:02:43 +0200

    adding storybook for react

--- packages/shared-ui/lib/index.js
+++ packages/shared-ui/lib/index.js
@@ -1,7 +1,11 @@
 "use strict";
 
-module.exports = components;
+import Button from "./button";
 
 function components() {
     console.log("Front components package!");
 }
+module.exports = {
+    components,
+    Button
+};
